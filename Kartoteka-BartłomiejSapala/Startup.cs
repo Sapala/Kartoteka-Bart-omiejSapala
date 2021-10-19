a90cec853bbc8072b7262ea1c9376d1d9786d5f7
Bartłomiej Sapała <bartlomiej.sapala@ibcs.pl>
2021-10-19 12:38:40 +0200

    test jenkins

--- Kartoteka-BartłomiejSapala/Startup.cs
+++ Kartoteka-BartłomiejSapala/Startup.cs
@@ -26,7 +26,7 @@ namespace Kartoteka_BartłomiejSapala
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews();a
             services.AddDbContext<Context>(options => options.UseSqlServer(Configuration.GetConnectionString("Context")));
         }
 
